a16005e198000ed9f90a98abac49079096761f64
Wojtek Erbetowski <wojciech@erbetowski.pl>
2012-12-31 15:52:29 +0100

    Implemented plastic code. Still throwing exceptions tough

--- src/test/groovy/pl/erbetowski/plastic/logging/LoggingSpec.groovy
+++ src/test/groovy/pl/erbetowski/plastic/logging/LoggingSpec.groovy
@@ -1,5 +1,11 @@
 package pl.erbetowski.plastic.logging
 
+import org.apache.tapestry5.internal.plastic.StandardDelegate
+import org.apache.tapestry5.plastic.MethodAdvice
+import org.apache.tapestry5.plastic.MethodInvocation
+import org.apache.tapestry5.plastic.PlasticClass
+import org.apache.tapestry5.plastic.PlasticClassTransformer
+import org.apache.tapestry5.plastic.PlasticManager
 import org.slf4j.Logger
 import spock.lang.Ignore
 import spock.lang.Specification
@@ -20,7 +26,34 @@ class LoggingSpec extends Specification {
         1 * logger.debug("Entering method") // should log once
     }
 
-    @Ignore def prepareServiceWithLogger(def logger) {
-        new FakeService()
+    @Ignore def prepareServiceWithLogger(Logger logger) {
+
+        final def advice = new MethodAdvice() {
+            @Override
+            void advise(MethodInvocation invocation) {
+                logger.debug("Entering method")
+                invocation.proceed()
+            }
+        }
+        
+        def transformer = new PlasticClassTransformer() {
+            @Override
+            void transform(PlasticClass plasticClass) {
+                def methods = plasticClass.getMethods()
+
+                methods[0].addAdvice()
+            }
+        }
+
+        def delegate = new StandardDelegate(transformer)
+        def plasticManager = PlasticManager
+                .withContextClassLoader()
+                .packages(['pl.erbetowski.plastic.logging'])
+                .delegate(delegate)
+                .create()
+
+        def instantiator = plasticManager.getClassInstantiator('pl.erbetowski.plastic.logging.FakeService')
+
+        return instantiator.newInstance()
     }
 }
